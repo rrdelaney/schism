28b1af36c43d1c43d9ab062d8452e26c23fc464b
Ryan Delaney <rrdelaney@outlook.com>
2016-07-14 01:31:48 -0400

    Update fn.js

--- fn.js
+++ fn.js
@@ -1,4 +1,4 @@
 const route = require('./route')
 
 module.exports = (method, path) => handler =>
-  route[method](path)(e => e.state.body.then(handler))
+  route[method](path)(e => e.state.body.then(handler).then(body => ({ body })))
